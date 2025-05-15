d574f3f0a8cb0a29b29e313fee951b12ee929ef9
TuanAnh2210 <dh52200332@student.stu.edu.vn>
2025-05-16 05:38:00 +0700

    Initial commit: Web + JSON Server

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
-const path = require('path');
-const jsonServer = require('json-server');
+const path = require("path");
+const jsonServer = require("json-server");
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '../json-server-db/db.json'));
+const router = jsonServer.router(path.join(__dirname, "../json-server-db/db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
